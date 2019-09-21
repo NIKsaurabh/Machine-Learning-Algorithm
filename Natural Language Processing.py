09db08cd3f7cae6bdfd9232117fc1c0a0bd8c74a
Saurabh Kumar <51809462+NIKsaurabh@users.noreply.github.com>
2019-09-21 21:20:41 +0530

    Add files via upload

---
+++ Natural Language Processing.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 16:05:28 2019
+
+@author: saurabh
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing the dataset
+dataset=pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+#cleaning the texts
+import re
+#import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus=[]
+for i in range(0,1000):
+    review=re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review=review.lower()
+    review=review.split()
+    ps=PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review=' '.join(review)
+    corpus.append(review)
+
+#creating the bag of word model
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y=dataset.iloc[:,1].values
+
+#splitting dataset in training and test set
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
+#fitting Naive Bayes to the training set
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+classifier.fit(X_train,y_train)
+
+#predicting the test set result
+y_pred=classifier.predict(X_test)
+
+#mating confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+
